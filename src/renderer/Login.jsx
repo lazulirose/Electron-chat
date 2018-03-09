40a62d6de8bc1ee4bcfb7849f97815b05ecf59e7
Ancy <lazuliroseancy@gmail.com>
2018-03-09 23:06:14 +0900

    Fix Login.jsx

--- src/renderer/Login.jsx
+++ src/renderer/Login.jsx
@@ -1,96 +1,101 @@
 import React from "react";
-import {Link, hashHistory} from "react-router";
+import { Link, hashHistory } from "react-router";
 import Errors from "./Errors";
 import firebase from "firebase/firebase-browser";
 
-const FORM_STYLE = {
-    margin: "0 auto",
+const SIGNUP_FORM_STYLE = {
+    margin: "0, auto",    
     padding: 30
 };
 
-const SIGNUP_LINK_STYLE = {
-    display: "inline-block",
+const CANCEL_BUTTON_STYLE = {
     marginLeft: 10
-}
+};
 
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: localStorage.userEmail || "",
+            email:  localStorage.userEmail || "",
             password: localStorage.userPassword || "",
-            errors: [],
-        };    
-    this.handleOnChangeEmail.bind(this);
-    this.handleOnChangePassword.bind(this);
-    this.handleOnChangeSubmit.bind(this);
+            errors: []
+        };
+
+        this.handleOnChangeEmail = this.handleOnChangeEmail.bind(this);
+        this.handleOnChangePassword = this.handleOnChangePassword.bind(this);
+        this.handleOnSubmit = this.handleOnSubmit.bind(this);        
     }
-    
+
     handleOnChangeEmail(e) {
         this.setState({ email: e.target.value });
     }
+
     handleOnChangePassword(e) {
         this.setState({ password: e.target.value });
     }
 
     handleOnSubmit(e) {
-        const { email, password } = this.state;
+        const { email, password, name, photoURL } = this.state;
         const errors = [];
         let isValid = true;
         e.preventDefault();
+
         if (!email.length) {
             isValid = false;
-            errors.push("Email can't be blank.");
+            errors.push("Email address can't be blank.");
         }
+
         if (!password.length) {
             isValid = false;
             errors.push("Password can't be blank.");
         }
+        
         if (!isValid) {
             this.setState({ errors });
             return;
         }
-        
-        firebase.auth().signInWithEmailAndPassword(email, password).than(() => {
+
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
             localStorage.userEmail = email;
-            localStorage.userPassword = password;
+            ocalStorage.userPassword = password;
             hashHistory.push("/rooms");
         }).catch(() => {
-            this.setState({ errors: ["Incorrect email or password."]}); 
+            this.setState({ errors: ["Incorrect email or password."] });
         });
     }
-        
+
     render() {
-        return ( 
-            <form style={FROM_STYLE} onSubmit={this.handleOnSumit}>
-            <Errors errorMessage={this.state.errors}/>
-            <div className="form-group">
-                <label for="">Email address</label>
-                <input 
-                type="email"
-                className="form-control"
-                placeholder="email"
-                onChange={this.handleOnChangeEmail}
-                value={this.state.email}
-                />
-            </div>
-            <div className="form-group">
-                <label for="">Password</label>
-                <input 
-                type="password"
-                className="form-control"
-                placeholder="password"
-                onChange={this.handleOnChangePassword}
-                value={this.state.password}
-                />
-            </div>
+        return (
+            <form style={SIGNUP_FORM_STYLE} onSubmit={this.handleOnSubmit}>
+                <Errors errorMessages={this.state.errors} />
                 <div className="form-group">
-                    <button className="btn btn-large btn-default">Login</button>
-                    <div style={SIGNUP_LINK_STYLE}>
-                        <Link to="/signup">Create new account!</Link>
-                    </div>
+                    <label>Email address*</label>
+                    <input 
+                        type="email"
+                        className="form-control"
+                        placeholder="email"
+                        value={this.state.email}
+                        onChange={this.handleOnChangeEmail}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input 
+                        type="password"
+                        className="form-control"
+                        placeholder="password"
+                        value={this.state.password}
+                        onChange={this.handleOnChangePassword}
+                    />
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-large btn-primary">Create new account</button>
+                    <Link to="/login">
+                        <button type="button" style={CANCEL_BUTTON_STYLE} className="btn btn-large btn-default">Cancel</button>                
+                    </Link>                    
                 </div>
             </form>
+
         );
     }
 }
